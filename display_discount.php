0689cf33f00e9fde85550b0d2cb857ac646ac992
Sumathi Selvaraj <ss3467@afsconnect1.njit.edu>
2017-02-12 23:03:16 -0500

    Filter input

--- display_discount.php
+++ display_discount.php
@@ -1,7 +1,9 @@
 <?php
+//Filter input 
 $product_description = filter_input(INPUT_POST, 'product_description');
 $list_price = filter_input(INPUT_POST, 'list_price');
 $discount_percent = filter_input(INPUT_POST, 'discount_percent');
+
 ?>
 
 <!DOCTYPE html>
